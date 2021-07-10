b7b81dd554b9411cb2d3aa284af16b16de1d4b0b
giadev25 <argiamahya1602@gmail.com>
2021-07-10 17:34:34 +0700

    Belajar animasi pertama selesai

---
+++ 2D_Animation_learn/Assets/script/PlayerMovement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float speed;
+    public Animator animator;
+    Rigidbody2D rb;
+
+    Vector2 movement;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement.x = Input.GetAxis("Horizontal");
+        movement.y = Input.GetAxis("Vertical");
+
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("speed", movement.SqrMagnitude());
+
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime);    
+    }
+}
